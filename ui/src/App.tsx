2e99c65fa1cb41b1983df59032fb5e930193a357
Shan-chaudhari <136202215+Shan-chaudhari@users.noreply.github.com>
2024-06-16 12:44:08 -0400

    moved all styles into a new file

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,4 @@
-import HomePage from "./components/HomePage";
+import HomePage from "./components/Home/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
